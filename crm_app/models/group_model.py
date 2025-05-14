789dc72e339df1ce7899bb5436175e8b1c602acd
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-14 20:55:02 +0500

    initial commit

--- crm_app/models/group_model.py
+++ crm_app/models/group_model.py
@@ -17,27 +17,10 @@ class Rooms(BaseModel):
         return self.title
 
 
-class TableType(BaseModel):
-    title = models.CharField(max_length=50)
-    descriptions = models.CharField(max_length=500, null=True, blank=True)
-
-    def __str__(self):
-        return self.title
-
-
-class Table(BaseModel):
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-    room = models.ForeignKey(Rooms, on_delete=models.RESTRICT)
-    type = models.ForeignKey(TableType, on_delete=models.RESTRICT)
-    descriptions = models.CharField(max_length=500, null=True, blank=True)
-
-
 class GroupStudent(BaseModel):
     title = models.CharField(max_length=25, unique=True)
     course = models.ForeignKey(Course, on_delete=models.RESTRICT, related_name='groups')
     teacher = models.ManyToManyField(Teacher, related_name="groups_taught")
-    table = models.ForeignKey(Table, on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
     descriptions = models.CharField(max_length=500, null=True, blank=True)
--- crm_app/serializers/group_serializer.py
+++ crm_app/serializers/group_serializer.py
@@ -1,29 +1,13 @@
 from rest_framework import serializers  # DRF serializerlarini yaratish uchun modul
 from ..models import *  # Serializer uchun kerak bo'lgan modullar
 
+
 # Day modeli uchun serializer
 class DaySerializer(serializers.ModelSerializer):
     class Meta:
         model = Day  # Model nomi
-        fields = ['id', 'title', 'descriptions'] # Seriyalizatsiya qilinadigan maydonlar
+        fields = ['id', 'title', 'descriptions']  # Seriyalizatsiya qilinadigan maydonlar
 
-# Rooms modeli uchun serializer
-class RoomsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Rooms  # Model nomi
-        fields = ['id', 'title', 'descriptions'] # Seriyalizatsiya qilinadigan maydonlar
-
-# TableType modeli uchun serializer
-class TableTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TableType  # Model nomi
-        fields = ['id', 'title', 'descriptions'] # Seriyalizatsiya qilinadigan maydonlar
-
-# Table modeli uchun serializer
-class TableSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Table  # Model nomi
-        fields = ['id', 'start_time', 'end_time', 'room', 'type', 'descriptions']
 
 # GroupStudent modeli uchun serializer
 from rest_framework import serializers
@@ -32,4 +16,4 @@ from rest_framework import serializers
 class GroupStudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = GroupStudent
-        fields = ['id', 'title', 'course', 'teacher', 'table', 'start_date', 'end_date', 'descriptions']
+        fields = ['id', 'title', 'course', 'teacher', 'start_date', 'end_date', 'descriptions']
