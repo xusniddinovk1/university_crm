294d63c238388ec3af6bed4f8990b89f12806e33
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-15 10:11:36 +0500

    initial commit

--- crm_app/models/group_model.py
+++ crm_app/models/group_model.py
@@ -9,14 +9,6 @@ class Day(models.Model):
         return self.title
 
 
-class Rooms(BaseModel):
-    title = models.CharField(max_length=50)
-    descriptions = models.CharField(max_length=500, null=True, blank=True)
-
-    def __str__(self):
-        return self.title
-
-
 class GroupStudent(BaseModel):
     title = models.CharField(max_length=25, unique=True)
     course = models.ForeignKey(Course, on_delete=models.RESTRICT, related_name='groups')
--- crm_app/models/lesson_model.py
+++ crm_app/models/lesson_model.py
@@ -1,4 +1,3 @@
-from django.db import models
 from .group_model import *
 
 
