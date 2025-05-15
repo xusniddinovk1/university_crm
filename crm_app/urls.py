0a522cad454d864f6cb5daa12c3858bb49a1f1da
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-15 10:13:47 +0500

    update urls.py

--- crm_app/urls.py
+++ crm_app/urls.py
@@ -14,28 +14,33 @@ from .views.teacher_view import TeacherListCreateView, TeacherDetailView
 
 router = DefaultRouter()
 router.register(r'payment', PaymentViewSet, basename='payment')
-router.register(r'department', DepartmentsViewSet, 'department')
-router.register(r'course', CourseViewSet, 'course')
-router.register(r'teacher-homeworks', TeacherHomeworkViewSet, 'teacher-homework')
+router.register(r'department', DepartmentsViewSet, basename='department')
+router.register(r'course', CourseViewSet, basename='course')
+router.register(r'teacher-homeworks', TeacherHomeworkViewSet, basename='teacher-homework')
 router.register(r'groups', GroupStudentViewSet, basename='groups')
-router.register(r'teacher-groups', TeacherGroupViewSet, 'teacher-groups')
-router.register(r'student-groups', StudentGroupViewSet, 'student-groups')
+router.register(r'teacher-groups', TeacherGroupViewSet, basename='teacher-groups')
+router.register(r'student-groups', StudentGroupViewSet, basename='student-groups')
 
 urlpatterns = [
     path('', include(router.urls)),
 
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+
     path('register/', RegisterUserApi.as_view(), name='register'),
     path('teachers/', TeacherListCreateView.as_view(), name='teacher-list-create'),
     path('teachers/<int:pk>/', TeacherDetailView.as_view(), name='teacher_detail'),
+
     path('students/', StudentListView.as_view(), name='student-list'),
     path('students/create/', StudentCreateView.as_view(), name='student-create'),
     path('students/<int:pk>/', StudentDetailView.as_view(), name='student-detail'),
     path('students/statistics/', StudentStatisticsView.as_view(), name='student-statistics'),
     path('student/homework/', StudentHomeworkView.as_view(), name='student-homework'),
+
     path('attendance/', GroupAttendanceView.as_view(), name='attendance'),
+
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
