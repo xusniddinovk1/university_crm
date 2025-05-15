0425d7597e6fc17269197a606ef8a9744d083e3b
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-05-15 10:17:12 +0500

    initial commit

--- crm_app/permissions.py
+++ crm_app/permissions.py
@@ -1,38 +1,31 @@
 from rest_framework.permissions import BasePermission
-from rest_framework.permissions import BasePermission
 
 
-class IsAdminOrStaff(BasePermission):  # Faqat adminlar va stafflar uchun ruxsat beruvchi sinf
-    def has_permission(self, request, view):  # Ruxsatni tekshirish metodi
-        # Foydalanuvchi autentifikatsiyadan o'tgan va adminlik va stafflik ruxsati ruxsati bor:
+class IsAdminOrStaff(BasePermission):  # [Only Staff user and Admin]
+    def has_permission(self, request, view):
+        # [User is authenticated and admin or staff user]
         return request.user and request.user.is_authenticated and (request.user.is_admin or request.user.is_staff)
 
 
-class IsTeacher(BasePermission):  # Teacherlikka tekshirish
+class IsTeacher(BasePermission):
     def has_permission(self, request, view):
-        # Foydalanuvchi autentifikatsiyadan o‘tganmi va u Teachermi:
         return request.user and request.user.is_authenticated and request.user.is_teacher
 
 
-class IsStudent(BasePermission):  # Studentlikka tekshirish
+class IsStudent(BasePermission):
     def has_permission(self, request, view):
-        # Foydalanuvchi autentifikatsiyadan o‘tganmi va u Studentmi:
         return request.user and request.user.is_authenticated and request.user.is_student
 
 
-# Teacher uchun faqat o'z guruhlariga tegishli amallarni qilaolishi uchun permission
 class IsTeacherOfGroup(BasePermission):
-    def has_permission(self, request, view):  # ruxsatni tekshiruvchi funksiya
-        # Foydalanuvchi autentifikatsiyadan o‘tganmi va u Teachermi
+    def has_permission(self, request, view):
         if not (request.user and request.user.is_authenticated and request.user.is_teacher):
-            return False  # Agar Teacher bo‘lmasa, ruxsat berilmaydi
+            return False
 
-        # Tegishli guruh IDsini olish
         group_id = request.data.get('group') or request.query_params.get('group')
-        if group_id:  # Agar group_id kiritilgan bo'lsa
-            # Teacherning guruhlari orasida shu ID li guruh borligini tekshiradi
+        if group_id:
             return request.user.teacher_profile.get_teacher.filter(id=group_id).exists()
-        return True  # agar guruh ID si kiritilmagan bo'lsa ruxsat beriladi(keyingi tekshirishga)
+        return True
 
 
 class IsAdminOrStaffOrStudentOrTeacher(BasePermission):
